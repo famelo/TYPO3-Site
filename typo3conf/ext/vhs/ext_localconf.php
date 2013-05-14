f97ca99a95445a34708dcb60e1da256e1a12b58d
Marc Neuhaus <mneuhaus@famelo.com>
2013-05-15 00:02:23 +0200

    Remove some static extensions

--- typo3conf/ext/vhs/ext_localconf.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'EXT:vhs/Classes/ViewHelpers/AssetViewHelper.php:Tx_Vhs_ViewHelpers_AssetViewHelper->buildAll';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_eofe'][] = 'EXT:vhs/Classes/ViewHelpers/AssetViewHelper.php:Tx_Vhs_ViewHelpers_AssetViewHelper->buildAllUncached';
