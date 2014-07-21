c4d9a24c230fbb8fca99aa67488cdb1459ef8559
Marc Neuhaus <mneuhaus@famelo.com>
2014-07-21 10:44:57 +0200

    Updated ws_less

--- typo3conf/ext/ws_less/ext_emconf.php
+++ typo3conf/ext/ws_less/ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "ws_less".
  *
- * Auto generated 14-11-2013 10:15
+ * Auto generated 21-07-2014 10:14
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,30 +14,19 @@ $EM_CONF[$_EXTKEY] = array (
 	'title' => 'Less compiler for TYPO3',
 	'description' => 'Compiles less files to CSS files.',
 	'category' => 'fe',
-	'shy' => 0,
-	'version' => '1.3.2',
-	'dependencies' => '',
-	'conflicts' => '',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
+	'version' => '1.3.4',
 	'state' => 'stable',
-	'uploadfolder' => 0,
+	'uploadfolder' => false,
 	'createDirs' => 'typo3temp/ws_less',
-	'modify_tables' => '',
-	'clearcacheonload' => 0,
-	'lockType' => '',
+	'clearcacheonload' => false,
 	'author' => 'Sven Wappler',
 	'author_email' => 'typo3YYYY@wapplersystems.de',
 	'author_company' => '',
-	'CGLcompliance' => NULL,
-	'CGLcompliance_note' => NULL,
 	'constraints' => 
 	array (
 		'depends' => 
 		array (
-			'typo3' => '6.0.0-6.1.99',
-			'' => '',
+			'typo3' => '6.0.0-6.2.99',
 		),
 		'conflicts' => 
 		array (
@@ -48,4 +37,3 @@ $EM_CONF[$_EXTKEY] = array (
 	),
 );
 
-?>
